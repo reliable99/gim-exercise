daa97900f8fe443c87c10a6423a99976f0bd614a
relaible99 <abdulmuhminsodiq@gmail.com>
2024-08-26 22:44:25 +0100

    update

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -11,7 +11,7 @@ const HeroBanner = () => (
       And Repeat
     </Typography>
     <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
-      Check out the most effective exercises personalized to you
+      Check out the most effective exercises personalized to you and look fit everytime everyday
     </Typography>
     <Stack>
       <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
